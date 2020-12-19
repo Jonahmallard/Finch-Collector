3bb128d134b5054e903a2eaecc023c0979d840fc
Jonah Mallard <benjaminjmallard@gmail.com>
2020-12-18 20:07:35 -0600

    Start feeding functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # import your models here
-from .models import Finch
+from .models import Finch, Feeding
 
 # Register your models here
 admin.site.register(Finch)
+# register the new Feeding Model 
+admin.site.register(Feeding)
